ecb6e24ae643bf5ec8ea8e76d07e6984db00f973
Tam S C <artamcrafts@hotmail.com>
2024-05-23 21:15:27 -0300

    EntregaFinal-TSC

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,21 +1,21 @@
 /* CAMBIOS REALIZADOS: ENTREGA FINAL */
 
 // Evitar que se sumen tareas vacías + cartel con innerText de aviso. (CHECK)
-// Librería SweetAlert2 para el aviso de mensaje de contacto enviado + asincronía con setTimeOut para cerrarlo (CHECK)
-// Librería Toastify para aviso de sesión iniciada (CHECK)
-// Corregido: "mensajes" array en vez de objeto (CHECK)
-// API sacada de rapidapi.com api de frases, categoría motivacionales + Fetch (CHECK)
+// Librería SweetAlert2 para el aviso de mensaje de contacto enviado + asincronía con setTimeOut para cerrarlo. (CHECK)
+// Librería Toastify para aviso de sesión iniciada. (CHECK)
+// Corregido: "mensajes" array en vez de objeto. (CHECK)
+// API sacada de rapidapi.com api de frases, categoría motivacionales + Fetch. (CHECK)
 // "API local", armado de .json con datos de los alumnos e imagen de certificado. (CHECK)
 
 ///////////////////////////////////////
 
 /* CAMBIOS REALIZADOS: PRE-ENTREGA 3 */
 
-// Info de materias con DATE en aside con innerText
-// Sumar storage y evento al inicio de sesión + devolver nombre en el bienvenido del aside
-// Listado de tareas pendientes (onclick, crear elementos y remove) + LOCALSTORAGE Y JSON
-// Planilla con checks para asistencias + notas con innerText con regularidad y promedio
-// SUGAR SYNTAX en saludo de bienvenida, en funcion promedio aprobado:desaprobado y en estado regular:irregular.
+// Info de materias con DATE en aside con innerText. (CHECK)
+// Sumar storage y evento al inicio de sesión + devolver nombre en el bienvenido del aside. (CHECK)
+// Listado de tareas pendientes (onclick, crear elementos y remove) + LOCALSTORAGE Y JSON. (CHECK)
+// Planilla con checks para asistencias + notas con innerText con regularidad y promedio. (CHECK)
+// SUGAR SYNTAX en saludo de bienvenida, en funcion promedio aprobado:desaprobado y en estado regular:irregular.(CHECK)
 
 
 //////////////////////////////////////// ASIDE 
@@ -42,7 +42,7 @@ fetch(URL)
     });
 
 
-////////// BIENVENIDO DOCENTE + inicio sesión con localStorage + sugar syntax
+////////// BIENVENIDO DOCENTE + inicio sesión con localStorage + sugar syntax + toastify
 
 function mostrarNombreDocente(){
     let nombreSesionDocente = document.getElementById("nombreDocente");
@@ -160,7 +160,7 @@ function mostrarTareas() {
 document.addEventListener("DOMContentLoaded", mostrarTareas);
 
 
-//////////////////////////////////////// FORMULARIO DE CONTACTO + asincronía con setTimeOut para el cierre del alert
+//////////////////////////////////////// FORMULARIO DE CONTACTO + asincronía con setTimeOut para el cierre del alert + SweetAlert2
 
 function enviarFormulario(event) {
     event.preventDefault(); //prevencion por defecto + en html estan los valores "requerido en cada campo"
@@ -204,7 +204,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return porcentajeAsistencia;
     }
 
-//PROMEDIO
+//PROMEDIO notas
     function calcularPromedioNotas() {
         let sumaNotas = 0;
         let cantidadNotas = 0;
@@ -217,7 +217,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return promedio;
     }
 
-//ESTADO PROMEDIO
+//PROMEDIO estado
     function calcularEstado(promedio) {
         return promedio >= 7 ? "Aprobado" : "Desaprobado";
     }
